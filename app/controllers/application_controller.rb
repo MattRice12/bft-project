df5e6f095ca56af3b2e973531359a1c025d56c65
Matt Rice <mattrice12@outlook.com>
2016-07-22 16:31:31 -0500

    heroku

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::API
   # protect_from_forgery with: :null_session
 
-  before_filter :current_user
+  # before_filter :current_user
 
   # API_MESSAGE = {not_found: {status: 404, message: "resource not found!"}}
 
