351fc8eb91ce1365129f2dd5b9d76315e576c8b0
Matt Rice <mattrice12@outlook.com>
2016-07-24 19:01:39 -0500

    fixing more cors stuff

--- config/application.rb
+++ config/application.rb
@@ -26,17 +26,11 @@ module BftProject
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
-  end
-end
-
-module YourApp
-  class Application < Rails::Application
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
         resource '*', :headers => :any, :methods => [:get, :post, :options, :put, :delete]
       end
     end
-
   end
 end
